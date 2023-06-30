8e53a805422f6cd29898c4f52af6bd1882401bf3
hatch-tyler <hatch.tyler@gmail.com>
2023-06-29 22:22:53 -0700

    Add install_inteloneapihpckit.sh

---
+++ install_inteloneapihpckit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# download the key to system keyring
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+
+# add signed entry to apt sources and configure the APT client to use Intel repository:
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+
+sudo apt update
+
+sudo apt install intel-basekit
+sudo apt install intel-hpckit
